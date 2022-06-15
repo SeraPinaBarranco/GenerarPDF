bbe500d230bbbadc54e340544fef4c74aa31ea4a
SerafinPiñaBarranco <arespin23@gmail.com>
2022-06-15 13:47:30 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php 
+    require_once "fpdf/fpdf.php";
+
+    $pdf = new FPDF();
+    $pdf->AddPage();
+    $pdf->SetFont('Arial','B',16);
+    $pdf->Cell(40,10,'¡Hola, Mundo!');
+    $pdf->Output();
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
